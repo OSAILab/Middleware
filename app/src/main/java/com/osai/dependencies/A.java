4912375cde563aff4bafbe38532371d9b499c770
13075633538 <as791144>
2023-07-03 11:44:34 +0800

    3.1.6

---
+++ app/src/main/java/com/osai/dependencies/A.java
@@ -0,0 +1,15 @@
+package com.osai.dependencies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class A extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
